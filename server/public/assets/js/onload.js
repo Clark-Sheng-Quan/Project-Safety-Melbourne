ff7e3a158512971529f4755fbb4f79c931111bd3
Clark <kukurishioiri@gmail.com>
2025-08-29 19:36:03 +1000

    skip login

--- server/public/assets/js/onload.js
+++ server/public/assets/js/onload.js
@@ -1,15 +1,15 @@
 // load and verify whether the token exsist
-window.onload = verifyLogin()
+// window.onload = verifyLogin()
 
-function verifyLogin(){
-  let token = localStorage.getItem("token")
-  if(!token){
-    window.location.href='./login.html'
-  }
-  return token
-}
+// function verifyLogin(){
+//   let token = localStorage.getItem("token")
+//   if(!token){
+//     window.location.href='./login.html'
+//   }
+//   return token
+// }
 
-function logout(){
-  localStorage.clear()
-  window.location.href='./login.html'
-}
+// function logout(){
+//   localStorage.clear()
+//   window.location.href='./login.html'
+// }
